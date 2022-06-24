0a7463c85c080aa1a3e534b8326fcc892315e167
ibrahim zeroual <ib.zeroual@outlook.com>
2022-06-24 19:09:43 +0100

    display table employees

--- src/app/_services/employee.service.ts
+++ src/app/_services/employee.service.ts
@@ -1,19 +1,37 @@
-import {Injectable, OnInit} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService  {
- employees : any;
+ employees = new Subject<any>();
+
 
   constructor(private http : HttpClient) { }
 
   getEmployees(){
     this.http.get("https://localhost:5001/api/Employer/all_employers").subscribe(data=>{
-      this.employees = data;
+     // this.employees = data;
+      this.employees.next(data);
+    });
+    console.log(this.employees);
+    return this.employees;
+  }
+  PostEmployee(Employe : any){
+    this.http.post("https://localhost:5001/api/Employer/add_employer",Employe).subscribe(data=>{
+      //this.employees = data;
+      console.log(Employe);
+    });
+  }
+  getOneEmployees(id : number){
+    this.http.get("https://localhost:5001/api/Employer/"+id).subscribe(data=>{
+      //this.employees = data;
       console.log(this.employees);
     });
   }
+
+
 
 }
