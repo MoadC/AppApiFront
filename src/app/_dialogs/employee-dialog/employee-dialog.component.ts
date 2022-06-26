e9fd37658741f3f9bd63bb417541fc57bb3402c2
ibrahim zeroual <ib.zeroual@outlook.com>
2022-06-26 11:19:21 +0100

    employee crud

--- src/app/_dialogs/employee-dialog/employee-dialog.component.ts
+++ src/app/_dialogs/employee-dialog/employee-dialog.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {EmployeeService} from "../../_services/employee.service";
+import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {Employee} from "../../_interfaces/employee";
 
 
 @Component({
@@ -11,9 +13,19 @@ import {EmployeeService} from "../../_services/employee.service";
 })
 export class EmployeeDialogComponent implements OnInit {
 
-  AddEmployeeForm: FormGroup;
+   editMode : boolean = false;
+   AddEmployeeForm: FormGroup;
+   First_Name ='';
+   Last_Name ='';
+   Gender ='';
+   Phone_Number ='';
+   EmployeeType ='';
+   Type_Equipe ='';
+   Email ='';
+   UserName ='';
+   Password ='';
 
-  constructor(private employeeService : EmployeeService) { }
+  constructor(private employeeService : EmployeeService,  @Inject(MAT_DIALOG_DATA) public data: any) { }
 
 
   ngOnInit(): void {
@@ -22,32 +34,43 @@ export class EmployeeDialogComponent implements OnInit {
 
 
   onSubmit() {
-    console.log(this.AddEmployeeForm.value);
-     this.employeeService.PostEmployee(this.AddEmployeeForm.value).subscribe();
-     this.employeeService.getEmployees();
+    if(this.editMode){
+       this.employeeService.UpdateEmployee(this.data.element.id,this.AddEmployeeForm.value).subscribe();
+       this.editMode=false;
+       this.ngOnInit();
+    }else {
+      this.employeeService.PostEmployee(this.AddEmployeeForm.value).subscribe();
+      this.ngOnInit();
+    }
   }
 
   initForm(){
-    let First_Name ='';
-    let Last_Name ='';
-    let Gender ='';
-    let Phone_Number ='';
-    let EmployeeType ='';
-    let Type_Equipe ='';
-    let Email ='';
-    let UserName ='';
-    let Password ='';
+    if(this.data) {
+
+      this.editMode = true;
+
+      this.First_Name =  this.data.element.firstName;
+      this.Last_Name = this.data.element.lastName;
+      this.Gender = this.data.element.gender;
+      this.Phone_Number = this.data.element.phoneNumber;
+      this.EmployeeType = this.data.element.employerType;
+      this.Type_Equipe = this.data.element.typeEquipe;
+      this.Email = this.data.element.email;
+      this.UserName = this.data.element.userName;
+      this.Password = this.data.element.password;
+
+      }
 
     this.AddEmployeeForm = new FormGroup({
-      'firstName': new FormControl(First_Name, Validators.required),
-      'lastName': new FormControl(Last_Name, Validators.required),
-      'gender': new FormControl(Gender, Validators.required),
-      'phoneNumber': new FormControl(Phone_Number, Validators.required),
-      'employerType': new FormControl(EmployeeType, Validators.required),
-      'typeEquipe': new FormControl(Type_Equipe, Validators.required),
-      'email': new FormControl(Email, Validators.required),
-      'userName': new FormControl(UserName, Validators.required),
-      'password': new FormControl(Password, Validators.required),
+      'firstName': new FormControl(this.First_Name, Validators.required),
+      'lastName': new FormControl(this.Last_Name, Validators.required),
+      'gender': new FormControl(this.Gender, Validators.required),
+      'phoneNumber': new FormControl(this.Phone_Number, Validators.required),
+      'employerType': new FormControl(this.EmployeeType, Validators.required),
+      'typeEquipe': new FormControl(this.Type_Equipe, Validators.required),
+      'email': new FormControl(this.Email, Validators.required),
+      'userName': new FormControl(this.UserName, Validators.required),
+      'password': new FormControl(this.Password, Validators.required),
     });
   }
 }
